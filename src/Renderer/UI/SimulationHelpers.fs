1cd5c5fdcaa0e407bcaa1008f2a5f0d4e567546b
Archontis Pantelopoulos <59971511+apantelopoulos@users.noreply.github.com>
2023-07-04 18:57:02 +0300

    Cleanup MainView.fs

--- src/Renderer/UI/SimulationHelpers.fs
+++ src/Renderer/UI/SimulationHelpers.fs
@@ -4,8 +4,12 @@ open Fable.React
 open Fable.React.Props
 open Simulation
 open CommonTypes
+open ModelType
+open DrawModelType
+open Sheet.SheetInterface
 open NumberHelpers
 open CanvasStateAnalyser
+open DiagramStyle
 open Fulma
 
 /// transforms the DC Simulation results into the DC Table
@@ -228,6 +232,347 @@ let getDCEquationsTable equations =
     Table.table []
         (List.append tableFormat tableChildren)
     
-    //(vsEquations @ inductorEquations @ voltageDividerEquations)
     
+
+//---------------------------- RUN SIMULATION FUNCTION --------------------------------//
     
+let findInputNodeFromComp (nodeLst: (Component*int option) list list) compId =
+    nodeLst |> List.findIndex (List.exists (fun (c,i)->c.Id = compId && i = (Some 0)))
+
+let runSimulation (model:Model) dispatch = 
+    let CS = model.Sheet.GetCanvasState ()
+    match CS with
+    |[],[] -> 
+        dispatch ForceStopSim
+        dispatch <| SetGraphVisibility false
+    |_ ->
+        let canvasState = CS |> combineGrounds
+        let compsNo,connsNo= List.length (fst canvasState),List.length (snd canvasState)
+        match model.PrevCanvasStateSizes = (compsNo,connsNo) with
+        |true ->
+            match model.Sheet.UpdateSim && model.Sheet.SimulationRunning with
+            |false -> ()
+            |true -> 
+                match checkCanvasStateForErrors canvasState (model.SimSubTabVisible=TimeSim) with
+                    |[] ->
+                        ClosePropertiesNotification |> dispatch
+                        CircuitHasNoErrors |> dispatch
+                        SimulationUpdated |> dispatch
+                        let res,componentCurrents,nodeLst,dm = Simulation.modifiedNodalAnalysisDC canvasState model.PreviousDiodeModes
+                        //let equations = getDCEquations model.Sheet.DCSim (fst CS)
+                        UpdateVoltages (Array.toList res) |> dispatch
+                        UpdateCurrents componentCurrents |> dispatch
+                        UpdateDiodeModes dm |> dispatch
+                        UpdateDCSim {MNA=res;ComponentCurrents=componentCurrents;NodeList=nodeLst;Equations=[]} |> dispatch
+                        
+                        match res with
+                        |[||] -> 
+                            dispatch ForceStopSim
+                            dispatch CircuitHasErrors
+                            dispatch (SetPropertiesNotification (Notifications.errorPropsNotification "Unknown error in Circuit. Cannot run Simulation")) 
+                        |_ ->
+                            match model.SimSubTabVisible with
+                            |DCsim -> ()
+                            |ACsim -> 
+                                dispatch UpdateNodes
+                                dispatch <| ShowNodesOrVoltagesExplicitState Nodes
+                                if model.showGraphArea then
+                                    let outputNode = model.SimulationData.ACOutput |> Option.defaultValue "1" |> int
+                                    let inputSource = model.SimulationData.ACSource |> Option.defaultValue ""
+                                    let res = (frequencyResponse canvasState inputSource outputNode)
+                                    UpdateACSim res |> dispatch
+                                else ()
+                            |TimeSim ->
+                                dispatch UpdateNodes
+                                dispatch <| ShowNodesOrVoltagesExplicitState Nodes
+                                if model.showGraphArea then
+                                    let inputSource = model.SimulationData.TimeInput |> Option.defaultValue "VS1" 
+                                    let inputNode = inputSource |> findInputNodeFromComp nodeLst
+                                    let outputNode = model.SimulationData.TimeOutput |> Option.defaultValue "1" |> int
+                                    let timeSim = (transientAnalysis canvasState inputSource inputNode outputNode)
+                                    UpdateTimeSim timeSim |> dispatch
+                                else ()
+                    |err ->
+                        dispatch ForceStopSim
+                        dispatch CircuitHasErrors
+                        dispatch (SetPropertiesNotification (Notifications.errorPropsNotification err[0].Msg))
+                        dispatch (Sheet (DrawModelType.SheetT.Msg.Wire (BusWireT.Msg.ErrorWires err[0].ConnectionsAffected) ))
+                        dispatch (Sheet (DrawModelType.SheetT.Msg.Wire (BusWireT.Msg.Symbol (SymbolT.Msg.ErrorSymbols (err[0].ComponentsAffected,err[0].ComponentsAffected,false) ))))
+        |false ->
+            dispatch <| UpdateCanvasStateSizes (compsNo,connsNo)
+            dispatch ForceStopSim
+            //dispatch <| CircuitHasErrors
+            dispatch <| SetGraphVisibility false
+
+
+
+//---------------------------- SIMULATION SUB-TABS --------------------------------//
+
+
+let startStopSimDiv model dispatch = 
+    let startStopState,startStopStr,startStopMsg = if model.Sheet.SimulationRunning then IsDanger,"Stop",ForceStopSim else if model.Sheet.CanRunSimulation then IsPrimary,"Start",SafeStartSim else IsWarning,"Restart"+CommonTypes.restartSymbol,SafeStartSim
+    div [] [
+        Heading.h4 [] [str "Start/Stop Simulation"]
+        Button.button [
+            Button.Color startStopState
+            Button.OnClick (fun _-> 
+                dispatch startStopMsg
+                dispatch <| SetGraphVisibility false
+                dispatch <| UpdateNodes
+                
+                )
+        ] [str startStopStr]
+        br []
+        br []
+    ]        
+
+let createACSimSubTab model comps dispatch =
+    let sourceOptions =
+        comps
+        |> List.collect (fun c->
+            match c.Type with
+            |VoltageSource _ -> [option [Value (c.Id)] [str (c.Label)]]
+            |_ -> []
+        )
+
+    let outputOptions =
+        [1..((List.length model.Sheet.DCSim.NodeList)-1)]
+        |> List.collect (fun i ->
+            [option [Value (string i)] [str ("Node "+(string i))]]    
+        )
+
+    let isDisabled = 
+        if model.Sheet.CanRunSimulation && model.Sheet.SimulationRunning then
+            match model.SimulationData.ACSource,model.SimulationData.ACOutput with
+            |Some x,Some y when x<>"sel" && y<>"sel" -> false
+            |_ -> true
+        else true
+
+    let magButtonText = if model.SimulationData.ACMagInDB then "dB" else "Normal"
+    let freqButtonText = if model.SimulationData.ACFreqInHz then "Hz" else "rads/s"
+
+    div [Style [Margin "20px"]] 
+        [ 
+            startStopSimDiv model dispatch
+
+            div [Hidden (not model.Sheet.SimulationRunning)] [
+            Heading.h5 [] [str "Setup AC Simulation"]
+            div [Style [Width "50%"; Float FloatOptions.Left]] [
+                Label.label [] [ str "Input" ]
+                Label.label [ ]
+                    [Select.select []
+                    [ select [(OnChange(fun option -> SetSimulationACSource (Some option.Value) |> dispatch))]
+                        ([option [Value ("sel")] [str ("Select")]] @ sourceOptions)
+                        ]
+                    ]
+            
+                Label.label [] [ str "Output" ]
+                Label.label [ ]
+                    [Select.select []
+                    [ select 
+                        [(OnChange(fun option -> SetSimulationACOut (Some option.Value) |> dispatch))]
+                        ([option [Value ("sel")] [str ("Select")]] @ outputOptions)
+                        ]
+                    ]
+                ]
+            div [Style [Width "50%"; Float FloatOptions.Right]] [
+                Label.label [] [ str "Magnitude" ]
+                Label.label [ ]
+                    [Button.button [Button.OnClick (fun _ -> dispatch SetSimulationACInDB); Button.Color IsLight] [str magButtonText]]
+            
+                Label.label [] [ str "Frequency" ]
+                Label.label [ ]
+                    [
+                        Button.button [Button.OnClick (fun _ -> dispatch SetSimulationACInHz);Button.Color IsLight] [str freqButtonText] 
+                    ]
+                ]
+
+            div [Style [Color "White"]] [str "f"]
+            br []
+
+            Button.button 
+                [   Button.OnClick (fun _ -> 
+                        RunSim |> dispatch
+                        SetGraphVisibility true |> dispatch); 
+                        Button.Color IsPrimary;
+                        Button.Disabled isDisabled] 
+                [str "Show"]
+
+            ]
+            br []
+            div [] [str "AC Simulation explores how the circuit behaves over a wide range of frequencies. The plots produced by the AC Simulation represent the magnitude and the phase of the ratio (output_node/input_source). During simulation, all other sources are set to 0 and diodes are assumed to be in conducting mode."]
+            
+        ]
+
+let createTimeSimSubTab model comps dispatch =
+    let sourceOptions =
+        comps
+        |> List.collect (fun c->
+            match c.Type with
+            |VoltageSource _ -> [option [Value (c.Id)] [str (c.Label)]]
+            |_ -> []
+        )
+
+    let outputOptions =
+        [1..((List.length model.Sheet.DCSim.NodeList)-1)]
+        |> List.collect (fun i ->
+            [option [Value (string i)] [str ("Node "+(string i))]]    
+        )
+
+    let isDisabled = 
+        match model.SimulationData.TimeInput,model.SimulationData.TimeOutput with
+        |Some x,Some y when x<>"sel" && y<>"sel" -> 
+            match checkTimeSimConditions comps with
+            |[] -> false
+            |err ->
+                true
+        |_ -> true
+
+    
+    div [Style [Margin "20px"]] 
+        [ 
+            startStopSimDiv model dispatch
+            div [Hidden (not model.Sheet.SimulationRunning)] [
+                Heading.h4 [] [str "Setup Time Simulation"]
+                Label.label [] [ str "Input" ]
+                Label.label [ ]
+                    [Select.select []
+                    [ select [(OnChange(fun option -> SetSimulationTimeSource (Some option.Value) |> dispatch))]
+                        ([option [Value ("sel")] [str ("Select")]] @ sourceOptions)
+                        ]
+                    ]
+            
+                Label.label [] [ str "Output" ]
+                Label.label [ ]
+                    [Select.select [] [ select [(OnChange(fun option -> SetSimulationTimeOut (Some option.Value) |> dispatch))]
+                        ([option [Value ("sel")] [str ("Select")]] @ outputOptions)
+                        ]
+                    ]
+                br []
+                br []
+                Button.button 
+                    [   Button.OnClick (fun _ -> 
+                            RunSim |> dispatch
+                            SetGraphVisibility true |> dispatch); 
+                            Button.Color IsPrimary;
+                            Button.Disabled isDisabled] 
+                    [str "Show"]
+            ]
+            br []
+            div [] [str "Time Simulation explores how the circuit behaves over time using 200 timesteps. The plots produced represent the input and output voltages, along with the two signals (steady-state and transient) that form the output voltage. Time simulation currently supports a maximum of one Voltage Source and one Capacitor or Inductor."]
+            
+
+        ]
+
+let createDCSubTab model (comps,conns) dispatch =
+    let nodesVoltagesState = match model.Sheet.ShowNodesOrVoltages with |Neither -> IsDanger |Nodes -> IsWarning |Voltages -> IsPrimary
+    let currentsState = if model.Sheet.ShowCurrents then IsPrimary else IsDanger
+    div [Style [Margin "20px"]] 
+        [ 
+            let startStopState,startStopStr,startStopMsg = if model.Sheet.SimulationRunning then IsDanger,"Stop",ForceStopSim else if model.Sheet.CanRunSimulation then IsPrimary,"Start",SafeStartSim else IsWarning,"Restart"+CommonTypes.restartSymbol,SafeStartSim
+            Heading.h4 [] [str "Start/Stop Simulation"]
+            Button.button [
+            Button.Color startStopState
+            Button.OnClick (fun _-> 
+                dispatch startStopMsg
+                dispatch <| RunSim
+                dispatch <| SetGraphVisibility false
+                    )
+            ] [str startStopStr]
+            br []
+            br []
+            div [Hidden <| not model.Sheet.SimulationRunning] [
+                Heading.h5 [] [str "Adjust on-Canvas Elements"]
+                Button.button [Button.OnClick(fun _ -> ShowOrHideCurrents |> dispatch); Button.Color currentsState] [ str "Currents" ]
+                span [Style [Width "20px";Color "White"]] [str "asd"]
+                Button.button [ 
+                Button.OnClick(fun _ -> 
+                UpdateNodes |> dispatch
+                ShowNodesOrVoltages |> dispatch); Button.Color nodesVoltagesState] [ str "Nodes/Voltages" ]
+                                  
+                br []
+                br []
+                Heading.h5 [] [str "DC Results"]
+                div [] [
+
+                Menu.menu [Props [Class "py-1";]]  [
+                    details [Open true;OnClick (fun _ -> dispatch RunSim)] [
+                        summary [menuLabelStyle] [ str "Table Results" ]
+                        Menu.list [] [getDCTable model.Sheet.DCSim (model.Sheet.SimulationRunning && model.Sheet.CanRunSimulation) (comps,conns) ]
+                    ]
+                ]
+                
+                Menu.menu [Props [Class "py-1";]]  [
+                    details [Open false;OnClick (fun _ -> dispatch RunSim)] [
+                        summary [menuLabelStyle] [ str "Equations" ]
+                        Menu.list [] [(getDCEquationsTable model.Sheet.DCSim.Equations)]
+                    ]
+                ]
+
+                Menu.menu [Props [Class "py-1";]]  [
+                    let paramsDiv =
+                        match model.TheveninParams with
+                        |None -> null
+                        |Some par ->
+                            let asstr = (string par.Resistance) + ", " + (string par.Voltage) + ", " + (string par.Current)
+                            div [] [
+                                Table.table [] [
+                                    tr [] [
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str "Rth"]
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str ((string (System.Math.Round (par.Resistance,6)))+" "+omegaString)]
+                                    ]
+                                    tr [] [
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str "Vth"]
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str ((string (System.Math.Round (par.Voltage,6)))+" V")]
+                                    ]
+                                    tr [] [
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str "Ino"]
+                                    td [Style [Color "Black"; VerticalAlign "Middle"; WhiteSpace WhiteSpaceOptions.Pre]] [str ((string (System.Math.Round (par.Current,6)))+" A")]
+                                    ]
+                                    
+                                ]
+                                //str asstr
+                                
+                            ] 
+
+                                
+                    details [Open false;] [
+                        summary [menuLabelStyle] [ str "Thevenin/Norton" ]
+                        Menu.list [] [
+                            str "Select a component from the dropdown below to view the thevenin or norton parameters representing the equivalent circuit seen by the component"
+                            br []
+                            Select.select []
+                                [ 
+                                let compOptions = 
+                                    comps
+                                    |> List.filter (fun c->match c.Type with |Opamp |Ground -> false |_ -> true)    
+                                    |> List.collect (fun c->[option [Value (c.Id)] [str (c.Label)]])
+
+                                select 
+                                    [(OnChange(fun option -> SetSimulationTheveninComp (Some option.Value) |> dispatch))]
+                                    ([option [Value ("sel")] [str ("Select")]] @ compOptions)
+                                    ]
+                            Button.button [Button.Color IsPrimary;Button.OnClick(fun _ -> (dispatch SetSimulationTheveninParams))] [str "Find"]    
+                            paramsDiv
+                        ]
+                    ]
+                ]
+                    
+
+                //hack to avoid hidden results
+                div [Style [Height "100px"]] []
+            ]]
+        ]
+
+
+let viewSimSubTab canvasState model dispatch =
+    let comps',conns' = combineGrounds canvasState
+    //match checkCanvasStateForErrors (comps',conns') with
+    //|[] ->
+    match model.SimSubTabVisible with
+    | DCsim -> 
+        createDCSubTab model (comps',conns') dispatch 
+    | ACsim -> 
+        createACSimSubTab model comps' dispatch
+    | TimeSim ->
+        createTimeSimSubTab model comps' dispatch
